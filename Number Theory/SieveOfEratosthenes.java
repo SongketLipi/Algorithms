ca119e8eb3dc34f4d1ab35bf6d6af708d2cbde3c
showrav-ansary <showrav.ansary.bd@gmail.com>
2021-08-01 16:22:37 +0600

    Prime Numbers - Sieve of Eratosthenes

--- Number Theory/SieveOfEratosthenes.java
+++ Number Theory/SieveOfEratosthenes.java
@@ -42,6 +42,7 @@ public class SieveOfEratosthenes {
         
     }
 
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         System.out.println("Enter the number : ");
