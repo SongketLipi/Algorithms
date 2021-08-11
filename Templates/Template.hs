cdeba6e47027db02a2df63b785626b5cad7289dc
showrav-ansary <showrav.ansary.bd@gmail.com>
2021-08-11 08:41:18 +0600

    Add Haskell Template

---
+++ Templates/Template.hs
@@ -0,0 +1,13 @@
+-- function signature
+-- functionName arg arg2 =
+    -- implement
+
+
+--uncomment and change accordingly
+--main :: IO () -- function signature
+--main = do
+    -- putStrLn "Enter Input :"
+    -- input <- getLine 
+    -- let x = (read input :: Int) -- do type conversion
+    -- putStrLn "Output is : " -- output
+    -- putStrLn ( show (function arg arg2 .. argn) :: String) -- computation output
